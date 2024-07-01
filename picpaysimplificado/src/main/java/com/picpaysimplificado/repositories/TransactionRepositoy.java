138bcdb410fc7cf2baaef5a03cfff53557eac5d9
Jean Victor S. <jeanvictor.san@gmail.com>
2024-07-01 13:38:16 -0300

    feat:create TransactionRepositoy

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/repositories/TransactionRepositoy.java
@@ -0,0 +1,7 @@
+package com.picpaysimplificado.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepositoy extends JpaRepository<Transaction, Long> {
+
+}
