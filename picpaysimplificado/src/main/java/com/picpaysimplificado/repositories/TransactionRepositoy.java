c7249e4aad5cd89815e11a98e1e0e21102fcd6d4
Jean Victor S. <jeanvictor.san@gmail.com>
2024-07-01 13:39:50 -0300

    fix:import Transaction Class to TransactionRepositoy

--- picpaysimplificado/src/main/java/com/picpaysimplificado/repositories/TransactionRepositoy.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/repositories/TransactionRepositoy.java
@@ -1,5 +1,7 @@
 package com.picpaysimplificado.repositories;
 
+import com.picpaysimplificado.domain.transaction.Transaction;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepositoy extends JpaRepository<Transaction, Long> {
