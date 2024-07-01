79933c454789b184687f7bb714cf70952b3fd91f
Jean Victor S. <jeanvictor.san@gmail.com>
2024-07-01 13:42:13 -0300

    fix: TransactionRepository Interface

--- picpaysimplificado/src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
@@ -4,6 +4,6 @@ import com.picpaysimplificado.domain.transaction.Transaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepositoy extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
 }
