05e8c7c063a024960ce08b4988f46e637691f9d0
Jean Victor S. <jeanvictor.san@gmail.com>
2024-07-01 21:00:45 -0300

    feat: Notification configs

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -0,0 +1,35 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+
+
+
+@Service
+
+public class NotificationService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception{
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+            System.out.println("erro ao enviar notificação");
+
+            throw new Exception("Serviço de notificação esta fora do ar");
+        }
+
+
+        System.out.println("NOTIFICAÇÃO ENVIADA PARA USUARIO");
+    }
+}
